9b0be3c23be5a8348027607923858b890a8ddedf
Marco Aurelio Guedes Junior <marco.guedes.jr@gmail.com>
2024-04-25 22:52:33 -0300

    Ajustes Rapidos
    Ajustes rapidos e importantes feitos no final da aula

--- JogoDaBateria/Assets/Script/Game/Game_Buttons.cs
+++ JogoDaBateria/Assets/Script/Game/Game_Buttons.cs
@@ -22,30 +22,38 @@ public class Game_Buttons : MonoBehaviour
     {
         if(Input.GetKeyDown(key))
         {
-            audio_source.PlayOneShot(note_sound[0]);
-            animator.SetTrigger("Clicou");
+            CLick();
+        }
+    }
 
-            switch (note)
+    public void Touch()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch[] touch = Input.touches;
+            Vector3[] touch_position = new Vector3[touch.Length];
+
+            RaycastHit[] hit = new RaycastHit[touch.Length];
+
+            for(int i = 0; i < touch.Length; i++)
             {
-                case Note.music_note.Chimbal:
-                    Sinal.Chimbal = true; break;
-                case Note.music_note.Caixa:
-                    Sinal.Caixa = true; break;
-                case Note.music_note.TomUm:
-                    Sinal.TomUm = true; break;
-                case Note.music_note.TomDois:
-                    Sinal.TomDois = true; break;
-                case Note.music_note.Bumbo:
-                    Sinal.Bumbo = true; break;
-                case Note.music_note.Surdo:
-                    Sinal.Surdo = true; break;
-                case Note.music_note.Prato:
-                    Sinal.Prato = true; break;
+                touch_position[i] = touch[i].position;
+            }
+
+            for(int i = 0; i < touch.Length; i++)
+            {
+                if (Physics.Raycast(Camera.main.transform.position, Camera.main.ScreenToWorldPoint(touch_position[i]), out hit[i]))
+                {
+                    if (hit[i].transform.tag == "Bateria")
+                    {
+                        hit[i].transform.gameObject.GetComponent<Game_Buttons>().CLick();
+                    }
+                }
             }
         }
     }
 
-    public void OnMouseDown()
+    public void CLick()
     {
         audio_source.PlayOneShot(note_sound[0]);
         animator.SetTrigger("Clicou");
@@ -67,6 +75,10 @@ public class Game_Buttons : MonoBehaviour
             case Note.music_note.Prato:
                 Sinal.Prato = true; break;
         }
+    }
 
+    public void OnMouseDown()
+    {
+        CLick();
     }
 }
